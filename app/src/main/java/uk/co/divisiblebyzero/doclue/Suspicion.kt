c6d6d4b503356c0394f4b5ca4aecae05a85ae9cb
Matthew Smalley <dev@divisiblebyzero.co.uk>
2019-07-19 19:26:03 +0100

    Added checkboxes and backing state

---
+++ app/src/main/java/uk/co/divisiblebyzero/doclue/Suspicion.kt
@@ -0,0 +1,5 @@
+package uk.co.divisiblebyzero.doclue
+
+data class Suspicion(val name: String, var state: Int = 0) {
+
+}
--- app/src/main/java/uk/co/divisiblebyzero/doclue/SuspicionsActivity.kt
+++ app/src/main/java/uk/co/divisiblebyzero/doclue/SuspicionsActivity.kt
@@ -1,10 +1,10 @@
 package uk.co.divisiblebyzero.doclue
 
 import android.graphics.Paint
-import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -27,32 +27,75 @@ class SuspicionsActivity : AppCompatActivity() {
         "Dartmoor"
     )
 
+    val suspicionsMap: MutableMap<String, Suspicion> = mutableMapOf()
+
     fun addNewTextViewUnderThisOne(
         layout: ConstraintLayout,
         aboveTextView: TextView,
         text: String,
-        size: Float
+        size: Float,
+        isHeader: Boolean = true
     ): TextView {
-        val set = ConstraintSet()
+
         val newTextView = TextView(this)
         newTextView.text = text
         newTextView.textSize = size
         newTextView.id = View.generateViewId()
 
-        newTextView.setOnClickListener { v -> if (v is TextView) v.paintFlags = (v.paintFlags xor Paint.STRIKE_THRU_TEXT_FLAG)}
+        newTextView.setOnClickListener { v ->
+            if (v is TextView) {
+                v.paintFlags = (v.paintFlags xor Paint.STRIKE_THRU_TEXT_FLAG)
+                if (v.text in suspicionsMap) suspicionsMap[v.text]!!.state = suspicionsMap[v.text]!!.state xor 1
+                Toast.makeText(this, suspicionsMap[v.text].toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
 
         layout.addView(newTextView)
+        val set = ConstraintSet()
         set.clone(layout)
         set.connect(newTextView.id, ConstraintSet.TOP, aboveTextView.id, ConstraintSet.BOTTOM, 32)
         set.connect(newTextView.id, ConstraintSet.LEFT, layout.id, ConstraintSet.LEFT, 16)
         set.applyTo(layout)
+
+
+
+        if (! isHeader) {
+
+            var rightAnchor: View = layout
+            for (i: Int in 1..5) {
+                val newCheckBox = CheckBox(this)
+                //newCheckBox.isChecked = false
+                newCheckBox.text = ""
+                newCheckBox.id = View.generateViewId()
+                newCheckBox.setOnClickListener { v ->
+                    if (v is CheckBox) {
+                        if (text in suspicionsMap) {
+                            suspicionsMap[text]!!.state = suspicionsMap[text]!!.state xor (1 shl i)
+                        }
+                        Toast.makeText(this, "Clicked: ${i} ${text}: ${suspicionsMap[text]}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                layout.addView(newCheckBox)
+                var checkBoxSet = ConstraintSet()
+                checkBoxSet.clone(layout)
+                checkBoxSet.connect(newCheckBox.id, ConstraintSet.TOP, aboveTextView.id, ConstraintSet.BOTTOM, 32)
+                checkBoxSet.connect(newCheckBox.id, ConstraintSet.RIGHT, rightAnchor.id, ConstraintSet.RIGHT, 64)
+                checkBoxSet.applyTo(layout)
+                rightAnchor = newCheckBox
+            }
+        }
+
+
+
+
         return newTextView
     }
 
     fun addTextGroup(layout: ConstraintLayout, lastTextView: TextView, title: String, items: Array<String>): TextView {
         var currentLast = addNewTextViewUnderThisOne(layout, lastTextView, title, 20f)
         for (item in items) {
-            currentLast = addNewTextViewUnderThisOne(layout, currentLast, item, 14f)
+            currentLast = addNewTextViewUnderThisOne(layout, currentLast, item, 14f, false)
+            suspicionsMap.put(item, Suspicion(item))
         }
         return currentLast
     }
